4b13fe8d9c5db8953b28bd97b6ecbaa2ff13adab
garciajam <156782768+garciajam@users.noreply.github.com>
2024-02-19 00:37:50 +0800

    Add files via upload

---
+++ CC31B-TeamGyatt/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Jamal";
+            CurrentPlayer.Gold = 100;
+            CurrentPlayer.CharacterClass = "Figter";
+            CurrentPlayer.HitPoints = 100;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = 0;
+            CurrentLocation.Name = "Hometown Square";
+            CurrentLocation.Description = "A peaceful village where your journey begins.";
+            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/home.png";
+            //CurrentLocation.ImageName = "../Engine/Images/Locations/home.jpg";
+
+        }
+    }
+}
